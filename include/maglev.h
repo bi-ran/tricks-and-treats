1bd37fa4ad35804e366e74ed3d8e3c66b378154f
Ran Bi <biran@mit.edu>
2019-06-30 02:19:40 -0400

    maglev: fast functions
    traditional dphi calculation, specialised invariant mass calculations.

---
+++ include/maglev.h
@@ -0,0 +1,26 @@
+#ifndef MAGLEV_H
+#define MAGLEV_H
+
+#include <array>
+#include <cmath>
+
+static constexpr auto ml_pi = std::acos(-1);
+
+template <typename T>
+T ml_dphi(T phi1, T phi2);
+
+enum coords { p3m, collider };
+
+template <coords U>
+struct turnout_t { };
+
+template <coords T>
+std::array<double, 2> ml_invariant_mass_pair(
+    double r1, double s1, double t1, double u1,
+    double r2, double s2, double t2, double u2);
+
+template <coords T>
+double ml_invariant_mass(double r1, double s1, double t1, double u1,
+                         double r2, double s2, double t2, double u2);
+
+#endif /* MAGLEV_H */
